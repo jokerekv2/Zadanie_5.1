3fdd8f6eb28761828f9ad453a409063ab84e201a
Dominik Mikolajczyk <jokerekv2@gmail.com>
2020-10-15 22:02:43 +0200

    rozwiązanie

--- src/Barman.java
+++ src/Barman.java
@@ -1,2 +1,8 @@
 public class Barman {
+    public void order(Person person, Drink drink) {
+        if ((person.getAge() < 18) && (drink.isAlcoholic() == true))
+            System.out.println(person.getName() + ", refusal to release the drink.");
+        else
+            System.out.println(person.getName() + ", your order has been accepted. Price: $" + drink.getPrice());
+    }
 }
